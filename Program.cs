17e6a9bd4ccd4b36154738039da28ecd4573c40a
johnsma <39930448+johnsma@users.noreply.github.com>
2018-06-18 18:34:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace hw14Plus
+{
+    class Program
+    {
+        //Public
+        public void Places() {
+
+            Console.WriteLine("I've been to Paris and Germany.");
+            Console.ReadKey();
+
+        }
+        //Private
+        private void States()
+        {
+            Console.WriteLine("I've Lived in Colorado, and Texas.");
+            Console.ReadKey();
+        }
+        //Public int
+        public int RetMuliInt()
+        {
+            int RetMultiInt = 25 * 25;
+            return RetMultiInt;
+         }
+        //Public with Parameters
+        public void home(String bedrooms, String bathrooms)
+        {
+            Console.WriteLine("I have two {0} and two {1}.", bedrooms, bathrooms);
+        }
+
+    }
+    }
+
+
